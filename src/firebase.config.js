b3a3f55674a2fa65573884dfaca715f45a8cebaf
vanshikaG <vanshikagautam22@gmail.com>
2022-12-05 11:12:02 +0530

    Explore, offer and Profile pages

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLWCkDcEkteqz53M4q3mnzvD5TVcnSsSY",
+  authDomain: "house-marketplace-app-1c1eb.firebaseapp.com",
+  projectId: "house-marketplace-app-1c1eb",
+  storageBucket: "house-marketplace-app-1c1eb.appspot.com",
+  messagingSenderId: "734056317288",
+  appId: "1:734056317288:web:1381d5f543c53c80e9d6dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
