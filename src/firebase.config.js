2a8d0e7e1bb28f3b8608b87d4eeb9bbafa3d62f8
vanshikaG <vanshikagautam22@gmail.com>
2022-12-17 10:44:12 +0530

    category and changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -13,6 +13,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -10,6 +10,7 @@ import ListingItem from '../components/ListingItem'
 function Category(){
     const [listings, setListings] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
     const params = useParams()
      
@@ -29,6 +30,9 @@ function Category(){
                 //Execute query 
                 const querySnap = await getDocs(q)
 
+                const lastVisible = querySnap.docs[querySnap.docs.length -1 ]
+                setLastFetchedListing(lastVisible)
+
                 const listings =[]
 
                 querySnap.forEach((doc)=>{
@@ -45,6 +49,42 @@ function Category(){
         }
         fetchListings()
     },[params.categoryName])
+
+
+    //Pagination / Load More
+    const onFetchMoreListings = async () =>{
+        try {
+            //Get reference
+            const listingsRef = collection(db,'listings')
+
+            //create a query
+            const q= query(
+                listingsRef, 
+                where('type', '==', params.categoryName), 
+                orderBy('timestamp', 'desc'),
+                startAfter(lastFetchedListing),
+                limit(10)
+                )
+            //Execute query 
+            const querySnap = await getDocs(q)
+
+            const lastVisible = querySnap.docs[querySnap.docs.length -1 ]
+            setLastFetchedListing(lastVisible)
+
+            const listings =[]
+
+            querySnap.forEach((doc)=>{
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            setListings((prevState) => [...prevState, ...listings])
+            setLoading(false)
+        } catch (error) {
+            toast.error('Could not fetch listings')
+        }
+    }
 
 
     return (
@@ -71,6 +111,14 @@ function Category(){
                     ))}
                 </ul>
             </main>
+
+            <br />
+            <br />
+
+            {lastFetchedListing && (
+                <p className="loadMore" onClick={onFetchMoreListings}>Load More </p>
+            )}
+
             </> 
             ) : (
                 <p> No listings for {params.categoryName}</p>
